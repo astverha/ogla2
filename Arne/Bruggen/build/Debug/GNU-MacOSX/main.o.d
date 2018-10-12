9ab24019b80e9a2f6e21aa9b2c013dfe05fd9c42
astverha <arne.verhaeghe@ugent.be>
2018-10-12 15:22:22 +0200

    georganiseerdererd

---
+++ Arne/Bruggen/build/Debug/GNU-MacOSX/main.o.d
@@ -0,0 +1,3 @@
+build/Debug/GNU-MacOSX/main.o: main.cpp bruggen.h
+
+bruggen.h:
---
+++ Arne/Rood-Zwarte-Bomen/cmake-build-debug/CMakeFiles/Rood_Zwarte_Bomen.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/arneverhaeghe/Documents/IW 4/IW 4/Semester 1/Algoritmen II/git/ogla2/Rood-Zwarte-Bomen/main.cpp" "/Users/arneverhaeghe/Documents/IW 4/IW 4/Semester 1/Algoritmen II/git/ogla2/Rood-Zwarte-Bomen/cmake-build-debug/CMakeFiles/Rood_Zwarte_Bomen.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
