0810fed7ce72cbe2af98949a555b01743846c2a6
astverha <arne.verhaeghe@ugent.be>
2018-10-18 08:43:44 +0200

    create btrees project

---
+++ Arne/03_BTrees/btree.h
@@ -0,0 +1,84 @@
+#ifndef BTREE_H
+#define BTREE_H
+
+#include "schijf.h"
+
+#include <iostream>
+
+using std::endl;
+using std::cerr;
+using std::ostream;
+
+// Dit bestand bevat code en hoofdingen van ...
+template<class T, class D, unsigned int m>
+class Btree;
+template<class T, class D, unsigned int m>
+class Bknoop;
+
+// Betekenis m: zie cursus
+
+template<class T, class D, unsigned int m>
+class Bknoop
+{
+    friend class Btree<T, D, m>;
+public:
+    Bknoop<T, D, m>()
+    {
+	// temp is nodig om te verhinderen dat de compiler dit negeert
+        int temp = (4 / (-4 + crashtest));
+        crashtest++;
+    };
+
+    const Bknoop<T, D, m>& operator=(const Bknoop<T, D, m>& b)
+    {
+        k = b.k;
+        isblad = b.isblad;
+
+        for (unsigned int i=0;i<k;i++)
+        {
+            sleutel[i] = b.sleutel[i];
+            data[i] = b.data[i];
+        }
+
+        if (!isblad)
+        {
+            for (unsigned int i=0; i<=k; i++)
+            {
+                index[i] = b.index[i];
+            }
+        }
+
+        return *this;
+    }
+
+    T sleutel[m];
+    D data[m];
+    blokindex index[m + 1];
+    unsigned int k;
+    bool isblad;
+
+private:
+    static int crashtest;
+};
+
+template<class T, class D, unsigned int m>
+int Bknoop<T, D, m>::crashtest = 0;
+
+template<class T, class D, unsigned int m>
+class Btree
+{
+    typedef Bknoop<T, D, m> Knoop;
+public:
+    Btree(Schijf<Knoop>& s):schijf(s){
+        wortel.k=0;
+        wortel.isblad=true;
+        wortelindex=schijf.schrijf(wortel);
+    }
+    
+private:
+   Schijf<Knoop>& schijf;
+   Knoop wortel;
+   blokindex wortelindex;
+};
+
+#endif
---
+++ Arne/03_BTrees/cmake-build-debug/CMakeFiles/03_BTrees.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/03_BTrees.dir/main.cpp.o"
+  "03_BTrees.pdb"
+  "03_BTrees"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/03_BTrees.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
