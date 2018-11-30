8fcaaaf5eaf8a7217c35b6ea5be719917c148360
astverha <arne.verhaeghe@ugent.be>
2018-11-30 15:42:12 +0100

    fh

--- Arne/08_ShiftAnd/main.cpp
+++ Arne/08_ShiftAnd/main.cpp
@@ -1,6 +1,42 @@
 #include <iostream>
 
+#include "shiftand.h"
+
+using namespace std;
+
+void print_queue(queue<const uchar*> q)
+{
+    cout << "inhoud queue: " << endl;
+    while (!q.empty())
+    {
+        cout << q.front() << " ";
+        q.pop();
+    }
+    cout << endl;
+}
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    cout << "Hello, World!" << endl << endl;
+
+    uchar* naald = (uchar*) "aap";
+    uint naaldLengte = 3;
+
+    Shiftand shifty = Shiftand(naald, naaldLengte);
+
+    cout << "Letterpatroon: " << endl;
+    for(int i=0; i< naaldLengte; i++){
+        cout << shifty.letterpatroon[naald[i]] << endl;
+    }
+    cout << endl;
+
+    uchar* hooiberg = (uchar*) "jan jaap van der aap";
+    uint hooibergLengte = 20;
+
+    queue<const uchar*> teVerwerkenUchars;
+
+    shifty.zoek(teVerwerkenUchars, hooiberg, hooibergLengte);
+
+    print_queue(teVerwerkenUchars);
+
     return 0;
 }
--- Arne/08_ShiftAnd/shiftand.h
+++ Arne/08_ShiftAnd/shiftand.h
@@ -1,16 +1,44 @@
 #include "bitpatroon.h"
 #include <queue>
+
+using namespace std;
+
 typedef unsigned int uint;
 typedef unsigned char uchar;
+
 class Shiftand{
 public:
-//naaldlengte moet kleiner dan patroonlengte zijn,
-//anders wordt een exceptie opgegooid.
-    Shiftand(const uchar* naald,uint _naaldlengte);
-    void zoek(std::queue<const uchar*>& plaats,
-                     const uchar* hooiberg, uint hooiberglengte);
-private:
+    //naaldlengte moet kleiner zijn dan patroonlengte, anders wordt een exceptie opgegooid.
+    Shiftand(const uchar* naald, uint _naaldlengte);
+
+    void zoek(queue<const uchar*>& plaats, const uchar* hooiberg, uint hooiberglengte);
+
     Bitpatroon letterpatroon[256];
     uint naaldlengte;
 };
-    
+
+Shiftand::Shiftand(const uchar *naald, uint _naaldlengte) {
+
+    for(int c=0; c<_naaldlengte; c++){
+        this->letterpatroon[naald[c]] = 0;
+    }
+
+    for(int i=0; i<_naaldlengte; i++){
+        this->letterpatroon[naald[i]] = this->letterpatroon[naald[i]] | (1 << i);
+    }
+
+    this->naaldlengte = _naaldlengte;
+}
+
+void Shiftand::zoek(queue<const uchar*>& plaats, const uchar* hooiberg, uint hooiberglengte){
+
+    Bitpatroon vorige(0);
+    int matcher = 1 << (this->naaldlengte-1);
+
+    for(int j=0; j<hooiberglengte; j++){
+        vorige = (this->letterpatroon[hooiberg[j]] | 0) & (vorige.shiftrechts(1) | 1);
+        if(vorige.en(matcher)){
+            plaats.push(&hooiberg[j - this->naaldlengte + 1]);
+        }
+    }
+}
